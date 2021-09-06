178bf65f41cec019c8b225641efecd50534f9fe7
Ahmed Ayman <ahmed.a.elkemary@gmail.com>
2021-09-06 18:18:23 +0200

    Adjust scripts file

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/AhmedAyman0/electron-releases
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+apt update
+apt install gh
 gh release create v1.0.0
